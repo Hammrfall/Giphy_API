ee0804f2bf6f3c82b268106528fefc32490e3c65
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-03-16 17:34:06 -0400

    add code to prevent duplication

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,12 @@
-var InitialButtonArray = ["Captain America", "Ironman", "Black Widow", "Hulk", "Hawkeye", "Thor"];
+var buttonArray = ["captain america", "ironman", "black widow", "hulk", "hawkeye", "thor"];
 var lastSearch;
-var searchString = "Captain America";
+var searchString = "";
 var currentGifCount = 0;
-var pullCount = 5;
-
+var startPullCount = 5;
+var pullCount = startPullCount;
 
 $(document).ready(function () {
-    InitialButtonArray.forEach(function (element) {
+    buttonArray.forEach(function (element) {
         addButton(element);
     })
 });
@@ -22,40 +22,59 @@ function addButton(name) {
 
 
 
-function getGifs (){
+function getGifs() {
     clearAllGifs();
     var siteString = "https://api.giphy.com/v1/gifs/search?"
     var apiKeyString = "api_key=4N43c1L84737DmVNlPdGssWrMq32sIeT"
-    var queryString = "&q=" + searchString + " &limit=25&offset=0&rating=PG&lang=en"
+    var queryString = "&q=" + searchString + "&limit=25&offset=0&rating=G&lang=en"
     var queryString = siteString + apiKeyString + queryString
-    $.ajax ({
+    $.ajax({
         url: queryString,
         method: "GET"
-    }).then (function(response) {
+    }).then(function (response) {
         console.log(response);
-        for (var i = 0;i < pullCount; i++){
-            addGif(response.data[i].images.fixed_width.url ,"g");
+        for (var i = 0; i < pullCount; i++) {
+            addGif(response.data[i].images.fixed_width.url, "g");
         }
 
     });
 }
 
-function addGif(url, rating){
+function addGif(url, rating) {
     var image = $("<img>");
     image.addClass("gif");
-    image.attr("src",url);
-    image.attr("alt","gif not available")
+    image.attr("src", url);
+    image.attr("alt", "gif not available")
     $("#gifcontainer").append(image);
 }
 
-function clearAllGifs (){
+function clearAllGifs() {
     $("#gifcontainer").empty();
 }
 
+function alreadyaButton(inputName) {
+    returnValue = false;
+    if (inputName === "") {
+        returnValue = true;
+    } else {
+        buttonArray.forEach(function (element) {
+            if (inputName === element) {returnValue = true;}
+        })
+    }
+    return returnValue;
+}
 
-$("#buttoncontainer").on ("click", ".search", function(){
+
+//Callbacks
+$("#buttoncontainer").on("click", ".search", function () {
     searchString = this.textContent
     getGifs();
 });
 
-
+$("#userinput").click('click', function () {
+    var inputString = $("#newbuttonname").val().trim().toLowerCase();
+    if (alreadyaButton(inputString) === false) {
+        addButton(inputString);
+        buttonArray.push(inputString);
+    }
+})
