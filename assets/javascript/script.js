d0f366220205d002677834c5537621665fe55618
Hammrfall <34660593+Hammrfall@users.noreply.github.com>
2018-03-17 16:53:30 -0400

    add toggle effect to gifs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,13 @@
 var buttonArray = ["captain america", "ironman", "black widow", "hulk", "hawkeye", "thor"];
 var lastSearch;
+//Global variables
 var searchString = "";
 var currentGifCount = 0;
-var startPullCount = 5;
+var startPullCount = 10;
 var pullCount = startPullCount;
 
 $(document).ready(function () {
-    buttonArray.forEach(function (element) {
+    buttonArray.forEach(function(element) {
         addButton(element);
     })
 });
@@ -20,8 +21,6 @@ function addButton(name) {
     $("#buttoncontainer").append(buttonItem);
 }
 
-
-
 function getGifs() {
     clearAllGifs();
     var siteString = "https://api.giphy.com/v1/gifs/search?"
@@ -34,17 +33,21 @@ function getGifs() {
     }).then(function (response) {
         console.log(response);
         for (var i = 0; i < pullCount; i++) {
-            addGif(response.data[i].images.fixed_width.url, "g");
+            addGif(response.data[i].images.fixed_width_still.url, response.data[i].images.fixed_width.url, response.data[i].rating);
         }
 
     });
 }
 
-function addGif(url, rating) {
+function addGif(staticURL, animatedURL, rating) {
     var image = $("<img>");
     image.addClass("gif");
-    image.attr("src", url);
-    image.attr("alt", "gif not available")
+    image.attr("src", staticURL)
+            .attr("data-staticurl", staticURL)
+                .attr("data-animatedurl",animatedURL)
+                    .attr("data-rating", rating)
+                        .attr("data-status", "static")
+                            .attr("alt", "gif not available");
     $("#gifcontainer").append(image);
 }
 
@@ -64,7 +67,6 @@ function alreadyaButton(inputName) {
     return returnValue;
 }
 
-
 //Callbacks
 $("#buttoncontainer").on("click", ".search", function () {
     searchString = this.textContent
@@ -78,3 +80,17 @@ $("#userinput").click('click', function () {
         buttonArray.push(inputString);
     }
 })
+
+$("#gifcontainer").on("click",".gif", function(){
+    console.log ($(this))
+    var state = $(this).attr("data-status")
+    if (state == "static") {
+        $(this).attr("src", $(this).attr("data-animatedurl"));
+        $(this).attr("data-status","animated");
+    }
+    else {
+        $(this).attr("src", $(this).attr("data-staticurl"));
+        $(this).attr("data-status","static");        
+    }    
+})
+
